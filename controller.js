96d6344d54aa092605137122975d52661fdfd6d1
Fell1pe <fellipe.lucas.zanin@gmail.com>
2024-08-08 08:06:30 -0300

    adiciona resto do java e corrige id do html

--- controller.js
+++ controller.js
@@ -2,35 +2,38 @@
 function acessar(){
     let loginEmail = document.getElementById('loginEmail').value;
     let loginSenha = document.getElementById('loginSenha').value;
-
+ 
     if(!loginEmail || !loginSenha){
-        alert('favor preencher todos os campos');
-    }
-    else{
-        //alert('campos preenchidos com sucesso');
+        alert('Favor preencher todos os campos');
+    }else{
+        alert('Campos preenchidos com sucesso');
+       
         window.location.href = 'cadastro.html';
     }
 }
-
-//função de criação de array para armazenamento de nomes
-
+ 
+//Função de criação do array para armazenamento de dados
+ 
 var dadosLista = [];
-
+ 
 function salvarUser(){
     let nomeUser = document.getElementById('nomeUser').value;
-
+ 
     if(nomeUser){
         dadosLista.push(nomeUser);
-        console.log(dadosLista);
-        document.getElementById('nomeUser').value = "";     
+        // console.log(dadosLista);
+        criarLista();
+        document.getElementById('nomeUser').value = "";
     }
     else{
-        alert("favor informar o nome");
+        alert("Favor informar o nome");
     }
 }
-
 // FUNÇÃO DE CRIAÇÃO DE LISTA
-
-function criaLista(){
-    let tabela = document;getElementById('tabela').innerHTML = "<tr><th>Nome Usuáiro</th><th>Ações</th></tr>";
+function criarLista(){
+    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
+    for(let i = 0; i <= (dadosLista.length -1); i++){ // A propriedade length tem como responsabilidade retornar a quantidade de caracteres de uma string ou o tamanho de um array. Caso a string ou o array esteja vazio, é retornado o valor 0.
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td></td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
+    }
 }
